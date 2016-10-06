2cb9806f08b46fdba3e6b82e55442f273569236b
Fábio Madeira <fbiomadeira@gmail.com>
2016-10-06 16:39:46 +0100

    Going back to previous method of loading to gh-pages

--- _deploy.sh
+++ _deploy.sh
@@ -1,81 +1,16 @@
 #!/bin/sh
 
-# set -e
-#
-# [ -z "${GITHUB_PAT}" ] && exit 0
-# [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-#
-# git config --global user.email "fbiomadeira@gmail.com"
-# git config --global user.name "Fábio Madeira"
-#
-# git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-# echo https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git
-# cd book-output
-# cp -r ../_book/* ./
-# git add --all *
-# git commit -m "Update the book" || true
-# git push origin gh-pages
+set -e
 
-set -e # Exit with nonzero exit code if anything fails
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+git config --global user.email "fbiomadeira@gmail.com"
+git config --global user.name "Fábio Madeira"
 
-# function doCompile {
-#   ./_compile.sh
-# }
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-# Run our compile script
-# doCompile
-cd out
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
 cp -r ../_book/* ./
-
-# Now let's go have some fun with the cloned repo
-git config user.name "Fábio Madeira"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_e5dad7a5dcf1_key -iv $encrypted_e5dad7a5dcf1_iv -in deploy_key.enc -out settings/keys/deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git add --all *
+git commit -m "Update the book" || true
+git push origin gh-pages
